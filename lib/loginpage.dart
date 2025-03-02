c481fcfc53a3f915da9f8d3ed6f4f90f5f16757d
Hassan <ijeyleekrane@gmail.com>
2025-03-02 23:27:24 +0100

    second commit

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,9 +1,47 @@
 import 'package:flutter/material.dart';
+
 class Loginpage extends StatelessWidget {
   const Loginpage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        title: Text(
+          "Rapid Tech",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 16.0,
+          ),
+        ),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            "HomePage",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                //profile picture, headerDrawer
+                //list view Mylist of coloume icons
+              ],
+            ),
+          ),
+        )
+        ,
+      ),
+    );
   }
 }
